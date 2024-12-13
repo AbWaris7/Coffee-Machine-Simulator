f4816eb73153457096911a38ad048b0b6cc59b83
abdulwariskhan <hellokhan.ch@gmail.com>
2024-12-13 16:28:13 +0100

    Estimate the number of servings

--- src/CoffeeMachine.java
+++ src/CoffeeMachine.java
@@ -1,3 +1,4 @@
+import java.util.Random;
 import java.util.Scanner;
 
 public class CoffeeMachine {
@@ -6,14 +7,41 @@ public class CoffeeMachine {
 
         Scanner input = new Scanner(System.in);
 
+//        int cups = input.nextInt();
+//        int water = cups * 200;
+//        int milk = cups * 50;
+//        int coffeeBeans = cups * 15;
+
+        System.out.println("Write how many ml of water the coffee machine has: ");
+        int water = input.nextInt();
+        System.out.println("Write how many ml of milk the coffee machine has: ");
+        int milk = input.nextInt();
+        System.out.println("Write how many grams of coffee beans the coffee machine has: ");
+        int grams = input.nextInt();
         System.out.println("Write how many cups of coffee you will need: ");
         int cups = input.nextInt();
-        System.out.println("For "+cups+" cups of coffee you will need: ");
-        int water = cups * 200;
-        int milk = cups * 50;
-        int coffeeBeans = cups * 15;
-        System.out.println(water+" ml of water");
-        System.out.println(milk+" ml of milk");
-        System.out.println(coffeeBeans+" ml of coffee beans");
+
+        int amountOfWater = water / 200;
+        int amountOfMilk = milk / 50;
+        int amountOfGrams = grams / 15;
+        int totalCups = Math.min(amountOfWater, Math.min(amountOfMilk, amountOfGrams));
+
+
+         if(totalCups >= cups) {
+             System.out.println("Yes, I can make that amount of coffee");
+
+             if(totalCups > cups) {
+                 System.out.println(" (and even " + (totalCups - cups) + " more than that)");
+             }
+         } else {
+             System.out.println("No, I can only make " + totalCups + " cup(s) of coffee");
+         }
+
+
+
+
+
+
+
     }
 }
